3585d6cf560f97cfeb315a6288cac30656cfb4e7
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2024-09-02 21:14:20 -0400

    updated routes and styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// The bellow code (even though it looks commented out) is what is making the code run correctly when it comes to makeing JS calls
+// to either delete or update
+//= require rails-ujs
