df05b4f119514872ef2107c5c5992a5cd32e3087
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2024-09-04 17:38:47 -0400

    added a button that links to a route that will delete all checked items

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@rails/ujs";
+
 // The bellow code (even though it looks commented out) is what is making the code run correctly when it comes to makeing JS calls
 // to either delete or update
 //= require rails-ujs
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :lists
   root 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "home/trash_em"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  
 
   # Defines the root path route ("/")
   # root "posts#index"
