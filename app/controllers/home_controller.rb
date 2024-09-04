0045bfe997b11fba9d95224edbc34de1810c597b
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2024-09-04 17:50:02 -0400

    updated some styling and added form to delete all to dos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,8 @@ class HomeController < ApplicationController
         @trash_em = List.where(:completed => true).destroy_all
         redirect_to lists_url, notice: "All Marked Items Were Sucessfully Deleted"
     end 
+    def trash_em_all
+        @trash_em = List.destroy_all
+        redirect_to lists_url, notice: "All Items Were Sucessfully Deleted"
+    end 
 end 
