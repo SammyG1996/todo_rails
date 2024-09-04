599ab0eeb2d5b0f76a8f23d4cb03add4df54d15e
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2024-09-04 18:54:48 -0400

    updated app to have route for checking off items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,14 @@ class HomeController < ApplicationController
         @trash_em = List.destroy_all
         redirect_to lists_url, notice: "All Items Were Sucessfully Deleted"
     end 
+    def check_marker
+        # Gets the param that was submitted
+        param_value = params[:id]
+        # This gets the to do item from the DB and saved it in the @list param
+        @list = List.find(params[:id])
+        # This will then update the todo to have the "completed" column set to "true"
+        @list.update(completed: true) # or whatever update you need to perform
+        # Redirects back to lists
+        redirect_to lists_path, notice: 'Marker checked.'
+    end
 end 
